7ed02945b4abe3577e3e4f8afd737eaff95b97ab
arakinaoto <naoto8705@gmail.com>
2026-02-04 12:34:47 +0900

    add: ランダムな単語を取得する非同期関数と表示関数を実装

---
+++ script.js
@@ -0,0 +1,27 @@
+const RANDOM_WORDS_URL_API = 'https://jlpt-vocab-api.vercel.app/api/words/random';
+
+// 非同期でランダムな単語を取得する関数
+const GetRandomWord = async () => {
+    try {
+        const response = await fetch(RANDOM_WORDS_URL_API);
+        const data = await response.json();
+        // console.log(data.word);
+        // console.log(data.furigana);
+        return {
+            word: data.word,
+            furigana: data.furigana
+        };
+    } catch (error) {
+        console.error('Error fetching random word:', error);
+        return null;
+    }
+}
+
+// ランダムな文章を取得して表示する関数
+const RenderNextWord = async () => {
+    const data = await GetRandomWord();
+    if (data) {
+        console.log(`Word: ${data.word}, Furigana: ${data.furigana}`);
+    }
+}
+RenderNextWord();
